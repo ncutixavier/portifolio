793c9fb5e87513026d83ccf9cfaec6702b72733e
unknown <ncuti60@gmail.com>
2021-05-03 22:16:08 +0200

    custom footer

--- src/components/landing pages/Main.js
+++ src/components/landing pages/Main.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Home from './Home'
 import About from './About'
 import Service from './Service'
-import Footer from '../Footer'
+import Footer from './Footer'
 import Project from './Project'
 import Contact from './Contact'
 import '../../assets/scss/home.scss';
