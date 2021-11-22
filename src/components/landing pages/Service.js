3bf2f2abf808ae78abe4caedbef0b5f0580e63a9
ncutixavier <ncuti60@gmail.com>
2021-11-22 18:16:56 +0200

    update portifolio

--- src/components/landing pages/Service.js
+++ src/components/landing pages/Service.js
@@ -8,7 +8,7 @@ const services = [
     {
         id: 1,
         image: 'http://',
-        text: 'Developing website for the Internet either static or dynamic web application.',
+        text: 'Web app for the Internet either static or dynamic web application.',
         title: 'Web Development',
         icon: <PublicIcon className="ss-icon" />
     },
@@ -17,14 +17,14 @@ const services = [
         image: 'http://',
         icon: <PhoneIphoneIcon className="ss-icon" />,
         title: 'Mobile Application',
-        text: 'Developing software applications that run on a mobile device.'
+        text: 'Software applications that run on a mobile device.'
     },
     {
         id: 3,
         image: 'http://',
         icon: <DesktopWindowsIcon className="ss-icon" />,
         title: 'Desktop Application',
-        text: 'Developing software applications that run natively on operating systems like Windows.'
+        text: 'Software applications that run natively on operating systems like Windows.'
     }
 ]
 
